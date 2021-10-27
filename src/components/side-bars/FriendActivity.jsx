1a0bbef007d2f0ec110fe4be83dcaa771fd79a4c
John <jzlowie@gmail.com>
2021-10-27 17:09:59 -0600

    initial build

---
+++ src/components/side-bars/FriendActivity.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './FriendActivity.scss';
+
+function FriendActivity() {
+  return (
+    <div className="FriendActivity">
+
+    </div>
+  );
+}
+
+export default FriendActivity;

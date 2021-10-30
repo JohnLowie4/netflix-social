a8aa0f722b5ab3afa6bae18107cbca1311f7bbb9
John <jzlowie@gmail.com>
2021-10-30 16:19:20 -0600

    added fontawesome icons to all pages

--- src/components/side-bars/FriendActivity.jsx
+++ src/components/side-bars/FriendActivity.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
-import './FriendActivity.scss';
+import React from "react";
+import "./FriendActivity.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Friends from "./Friends";
 
 function FriendActivity() {
   return (
     <div className="FriendActivity">
-
+      <div className="header">
+        <span>Friend activity</span>
+        <FontAwesomeIcon icon="user-plus" className="f-icon" />
+      </div>
+      <div className="body">{/* <Friends /> */}</div>
     </div>
   );
 }
---
+++ src/components/side-bars/Friends.jsx
@@ -0,0 +1,5 @@
+import Friend from "./Friend";
+
+export default function Friends() {
+  return <h1>Friends</h1>;
+}
